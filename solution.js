d11e61f62be30968b1deb07b65b9781068013be2
Laura Wu <leemaiwu@gmail.com>
2023-06-12 15:16:17 -0600

    Completed with for loop

--- solution.js
+++ solution.js
@@ -1,3 +1,12 @@
-var number=function(array){
-
+const number=function(array){
+ let count = 1
+ let finalArr = []
+ for (let i = 0; i < array.length; i++) {
+    finalArr.push(`${count}: ${array[i]}`)
+    count++
+ }
+ return finalArr
 }
+
+console.log(number([]))
+console.log(number(['a', 'b', 'c']))
